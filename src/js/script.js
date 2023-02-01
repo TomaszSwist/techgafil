45b4c7c96cd9a9bf31aa409781fec314c8d5f504
Tomasz Świst <kontakt@tomaszswist.pl>
2023-02-01 19:20:56 +0100

    cookies added

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,15 @@
 const nav = document.querySelector('.nav_bg')
-const mobileMenuBars = document.querySelectorAll(
-	'.menu__mobile-menu--top,.menu__mobile-menu--middle,.menu__mobile-menu--bottom'
-)
-
+const mobileMenuBars = document.querySelectorAll('.menu__mobile-menu--top,.menu__mobile-menu--middle,.menu__mobile-menu--bottom')
 const mobileArea = document.querySelector('.menu__mobile-menu--area')
 const mobileMenu = document.querySelector('.menu__mobile-menu')
+
+
+const cookies = document.querySelector('.cookie')
+const cookiesBtn = document.querySelector('.cookie--btn')
+
+const checkCookiesAcceptance = () => {
+	localStorage.getItem('cookiesAccepted') ? cookies.classList.add('hide')
+}
 
 const handleNavColor = () => {
 	if (window.scrollY >= 144) {
@@ -28,7 +33,14 @@ const handleMenu = () => {
 }
 
 
-
+const hideCookies = () => {
+	localStorage.setItem('cookiesAccepted', true);
+	checkCookiesAcceptance();
+}
 
 window.addEventListener('scroll', handleNavColor)
 mobileMenu.addEventListener('click', handleMenu)
+
+
+cookiesBtn.addEventListener('click', hideCookies)
+checkCookiesAcceptance()
