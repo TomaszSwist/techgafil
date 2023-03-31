5dfd414c53d84dbb0d41eaf2d9451a24bcdcdedc
Tomasz Świst <kontakt@tomaszswist.pl>
2023-03-31 20:03:23 +0200

    users language detection added

---
+++ src/js/langs.js
@@ -0,0 +1,17 @@
+// const lang = {
+//     en {
+//     title: '',
+//     index__meta: ''
+//     products__meta: '',
+//     products__coolers-nnpb--h1: ''
+//     products__coolers-nnpb--p: '',
+//     'products__coolers-pb--h1': '',
+//     'products__coolers-pb--p': '',
+//     'products__coolers-nnpb--img': '',
+//     'products__coolers-nnpb--img2': '',
+//     'products__coolers-pb--img': '',
+//    'products__coolers-pb--img2': '',
+// }, de {
+    
+// }
+// }
--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,53 @@
 const nav = document.querySelector('.nav_bg')
-const mobileMenuBars = document.querySelectorAll('.menu__mobile-menu--top,.menu__mobile-menu--middle,.menu__mobile-menu--bottom')
+const mobileMenuBars = document.querySelectorAll(
+	'.menu__mobile-menu--top,.menu__mobile-menu--middle,.menu__mobile-menu--bottom'
+)
 const mobileArea = document.querySelector('.menu__mobile-menu--area')
 const mobileMenu = document.querySelector('.menu__mobile-menu')
-
 
 const cookies = document.querySelector('.cookie')
 const cookiesBtn = document.querySelector('.cookie--btn')
 
+const userLangFull = navigator.language || navigator.userLanguage
+const userLang = userLangFull.slice(0, 2)
+
+let toTranslate
+
+const translateSite = lang => {
+	if (lang == 'pl') {
+		console.log('reload');
+		window.location.reload()
+	} else {
+		toTranslate = document.querySelectorAll('data-src')
+		console.log(toTranslate);
+	}
+}
+
+if (localStorage.getItem('choosenLanguage') === null) {
+	if (userLang == 'pl') {
+	} else if (userLang == 'de') {
+		document.documentElement.setAttribute('lang', 'de')
+		translateSite('userLang')
+	} else {
+		document.documentElement.setAttribute('lang', 'en')
+		translateSite('userLang')
+	}
+} else {
+	if (userLang == 'pl') {
+	} else {
+		document.documentElement.setAttribute('lang', localStorage.getItem('choosenLanguage'))
+		translateSite(localStorage.getItem('choosenLanguage'))
+	}
+}
+
+const handleLangChange = lang => {
+	document.documentElement.setAttribute('lang', lang)
+	localStorage.setItem('choosenLanguage', lang)
+	translateSite(lang)
+}
+
 const checkCookiesAcceptance = () => {
-	localStorage.getItem('cookiesAccepted') ? cookies.classList.add('hide'): false;
+	localStorage.getItem('cookiesAccepted') ? cookies.classList.add('hide') : false
 }
 
 const handleNavColor = () => {
@@ -32,15 +71,13 @@ const handleMenu = () => {
 	mobileMenuBars[2].classList.toggle('bottom-show')
 }
 
-
 const hideCookies = () => {
-	localStorage.setItem('cookiesAccepted', true);
-	checkCookiesAcceptance();
+	localStorage.setItem('cookiesAccepted', true)
+	checkCookiesAcceptance()
 }
 
 window.addEventListener('scroll', handleNavColor)
 mobileMenu.addEventListener('click', handleMenu)
-
 
 cookiesBtn.addEventListener('click', hideCookies)
 checkCookiesAcceptance()
